c5c41eb390edfd11ca99a7acb6cd606309dba6cb
denzelw2 <denzelw2@gmail.com>
2018-01-28 09:34:52 -0500

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,31 @@
+//Login Page 
+
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>PowerHouse</title>
+<link rel="stylesheet"= href="css/style.css">
+</head>
+<body style= "background-color:#95a5a6">
+
+	<div id="main-wrapper">
+		<center><h2>PowerHouse</h2>
+			<img src="images/avatar2.png" class="avatar2"/>
+	    </center>
+	
+	
+		<form class="myform" action="index.php" method="post"><br>
+			<label><b>Username:</label><br>
+			<input type="text" class="inputvalues" placeholder="Type your username"/><br>
+			<label><b>Password:</label><br>
+			<input type="password" class="inputvalues" placeholder="Type your password"/><br>
+			<input type="submit" id="login_btn" value="Login"/> <br>
+			<input type="button" id="register_btn" value="Register"/>
+		</form
+	</div>
+
+</body>
+</html>
